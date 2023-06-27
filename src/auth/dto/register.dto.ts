a33e8b73e0c167c9157b58ae087107a3248f78b9
nikenhpsr <niken18003@mail.unpad.ac.id>
2023-06-27 17:19:28 +0700

    API done

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,12 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
   @ApiProperty()
   password: string;
-
-  @ApiProperty()
-  username: string;
 }
